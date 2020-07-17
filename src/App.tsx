50f2a564fc1471bea34cfb6c11a788ee5d353e13
tiddal <tiddal96@gmail.com>
2020-07-17 18:08:42 +0100

    check 6

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          More Changes
+          More Changes to test
           {' '}
           <code>src/App.tsx</code>
           {' '}
