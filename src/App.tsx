aabfa71bf6eb4b703e88bdfec433ca12d5ba989d
tiddal <tiddal96@gmail.com>
2020-07-17 19:38:03 +0100

    ci: remove emojis

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          More Changes to test git
+          More Changes to test git build message
           {' '}
           <code>src/App.tsx</code>
           {' '}
